7a38282d0045152329385bf29305a1081ca43dde
Heather Nuffer <zzzuzz@gmail.com>
2019-12-16 15:03:54 -0700

    Tests for Dashboard and Display added

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,44 @@
 // Test away
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+import '@testing-library/jest-dom/extend-expect';
+
+
+test('Gate defaults to unlocked', () => {
+    const { getByText } = render(<Dashboard/>);
+    getByText(/unlocked/i);
+    });
+
+test('Gate defaults to open', ()=> {
+    const { getByText } = render(<Dashboard />);
+    getByText(/open/i);
+});
+
+test('Gate cannot be closed or opened if it is locked', () => {
+    const { getByText, getByTestId } = render(<Dashboard />);
+    const gateButton = getByTestId('gateButton');
+    fireEvent.click(gateButton);
+    const lockButton = getByText(/lock gate/i);
+    fireEvent.click(lockButton);
+    getByText(/locked/i);
+    //fireEvent.click(gateButton);
+    expect(gateButton).toBeInTheDocument();
+    expect(gateButton).toHaveClass('toggle-btn');
+    expect(gateButton).toHaveAttribute('disabled');
+});
+
+test('Dashboard shows the controls', () => {
+    const { getByTestId } = render(<Dashboard />);
+    const controls = getByTestId('controls');
+    expect(controls).toBeInTheDocument();
+    expect(controls).toBeVisible();
+});
+
+test('Dashboard shows the display', () => {
+    const { getByTestId } = render(<Dashboard />);
+    const display = getByTestId('display');
+    expect(display).toBeInTheDocument();
+    expect(display).toBeVisible();
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,33 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Display from './Display';
+import Dashboard from '../dashboard/Dashboard';
+
+test('Dashboard shows the display', () => {
+    const { getByTestId } = render(<Display />);
+    const display = getByTestId('display');
+    expect(display).toBeInTheDocument();
+    expect(display).toBeVisible();
+});
+
+test('When closed, uses the red-led class', ()=> {
+    const { getByTestId } = render(<Dashboard />);
+    const gateButton = getByTestId('gateButton');
+    fireEvent.click(gateButton);
+    const closedStatusEl = getByTestId('closedStatus');
+    expect(closedStatusEl).toHaveClass('red-led');
+});
+
+test('When locked, uses the red-led class', ()=> {
+    const { getByTestId } = render(<Dashboard />);
+    //Close the gate first
+    const gateButton = getByTestId('gateButton');
+    fireEvent.click(gateButton);
+    //Then click lock button
+    const lockButton = getByTestId('lockButton');
+    fireEvent.click(lockButton);
+    const lockedStatusEl = getByTestId('lockedStatus');
+    expect(lockedStatusEl).toHaveClass('red-led');
+});
