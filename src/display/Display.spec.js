a8daf4b02eca426fbe5988047d51a3c4c4d36825
Heather Nuffer <zzzuzz@gmail.com>
2019-12-16 15:48:47 -0700

    Finished tests for Display component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -12,7 +12,7 @@ test('Dashboard shows the display', () => {
     expect(display).toBeVisible();
 });
 
-test('When closed, uses the red-led class', ()=> {
+test('When closed, uses the red-led class for the closed-status div', ()=> {
     const { getByTestId } = render(<Dashboard />);
     const gateButton = getByTestId('gateButton');
     fireEvent.click(gateButton);
@@ -20,7 +20,7 @@ test('When closed, uses the red-led class', ()=> {
     expect(closedStatusEl).toHaveClass('red-led');
 });
 
-test('When locked, uses the red-led class', ()=> {
+test('When locked, uses the red-led class for the locked-status div', ()=> {
     const { getByTestId } = render(<Dashboard />);
     //Close the gate first
     const gateButton = getByTestId('gateButton');
@@ -31,3 +31,41 @@ test('When locked, uses the red-led class', ()=> {
     const lockedStatusEl = getByTestId('lockedStatus');
     expect(lockedStatusEl).toHaveClass('red-led');
 });
+
+test('when open, uses the green-led class for the closed-status div', () => {
+    const { getByTestId, rerender } = render(<Display closed={false}/>);
+    const closedStatusEl = getByTestId('closedStatus');
+    expect(closedStatusEl).toHaveClass('green-led');
+    rerender(<Display closed={true} />);
+    expect(closedStatusEl).not.toHaveClass('green-led');
+});
+
+test('when unlocked, uses the green-led class for the locked-status div', () => {
+    const { getByTestId, rerender } = render(<Display locked={false}/>);
+    const lockedStatusEl = getByTestId('lockedStatus');
+    expect(lockedStatusEl).toHaveClass('green-led');
+    rerender(<Display locked={true} />);
+    expect(lockedStatusEl).not.toHaveClass('green-led');
+});
+
+test('displays Closed if the closed prop is true', () => {
+    const { getByText, rerender, queryByText} = render(<Display closed={true}/>);
+    getByText(/closed/i);
+    rerender(<Display closed={false} />);
+    getByText(/open/i);   
+});
+
+test('displays Closed if the closed prop is true and Open if otherwise', () => {
+    const { getByText, rerender } = render(<Display closed={true}/>);
+    getByText(/closed/i);
+    rerender(<Display closed={false} />);
+    getByText(/open/i);   
+});
+
+test('displays Locked if the locked prop is true and Unlocked if otherwise', () => {
+    const { getByText, rerender } = render(<Display locked={true}/>);
+    getByText(/^locked$/i);
+    rerender(<Display locked={false} />);
+    getByText(/unlocked/i);   
+});
+
